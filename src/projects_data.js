13f8a592a29afaab8d1c8d755c4f0e3a27a03c82
Kaung Myat Khaing <kaung8701@gmail.com>
2022-06-02 01:02:46 +0630

    add new resume file

---
+++ src/projects_data.js
@@ -0,0 +1,35 @@
+let project_data = [
+	{
+		title: "TodoMatic",
+		description:
+			"A reliable and accessible todo list creator made for everyone",
+		imgUrl: "https://i.imgur.com/BhJfZGf.png",
+		altText: "project demo img",
+		pageUrl: "https://kaung-myat-khaing.github.io/todomatic/",
+	},
+	{
+		title: "tenzies",
+		description:
+			"Roll 10 dice until all the dice has the same number. Compare your current score with your best score and try to beat it.",
+		imgUrl: "https://i.imgur.com/BFRsDfQ.png",
+		altText: "project demo img",
+		pageUrl: "https://kaung-myat-khaing.github.io/tenzies/",
+	},
+	{
+		title: "Quizzical",
+		description:
+			"A fun and interesting quiz game made possible with Open Trivia database API",
+		imgUrl: "https://i.imgur.com/L9I3OO1.png",
+		altText: "project demo img",
+		pageUrl: "https://kaung-myat-khaing.github.io/quizzical/",
+	},
+	{
+		title: "meme generator",
+		description:
+			"Create memes with your own idea and fill them in the top and bottom text field.",
+		imgUrl: "https://i.imgur.com/APtFMjz.png",
+		altText: "project demo img",
+		pageUrl: "https://kaung-myat-khaing.github.io/meme-generator/",
+	},
+];
+export default project_data;
